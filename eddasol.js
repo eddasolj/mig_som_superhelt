ce3cd694a78f098858ffab1178f8d4b1b640cc95
eddasolj <124664752+eddasolj@users.noreply.github.com>
2023-08-25 14:34:50 +0200

    final change before css

--- eddasol.js
+++ eddasol.js
@@ -7,17 +7,17 @@ function showsuperheros(alleMineSuperHelte) {
 }
 
 function showsuperhero(superhero) {
+  console.log(superhero);
   const template = document.querySelector("template").content;
   const copy = template.cloneNode(true);
-  copy.querySelector("h2").textContent = superhero.alians;
+  copy.querySelector("h2").textContent = superhero.alias;
   copy.querySelector("h3").textContent = superhero.realName;
   copy.querySelector("p.powers").textContent = superhero.powers;
   copy.querySelector("p.origin").textContent = superhero.origin;
-  copy.querySelector("p.weakness").textContent = superhero.weakness;
+  copy.querySelector("p.weakness").textContent = superhero.weaknesses;
   copy.querySelector("p.active").textContent = superhero.active;
   copy.querySelector("p.height").textContent = superhero.height;
-  copy.querySelector("p.birthday").textContent = superhero.birthday;
-  copy.querySelector("img").src = superhero.img;
+  copy.querySelector("img").src = superhero.image;
   const parent = document.querySelector(".grid");
   parent.appendChild(copy);
 }
