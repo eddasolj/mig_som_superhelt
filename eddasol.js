f61fccecceb9c147bc8a99649ca338f3bc837cec
eddasolj <124664752+eddasolj@users.noreply.github.com>
2023-08-25 12:44:32 +0200

    js

---
+++ eddasol.js
@@ -0,0 +1,17 @@
+{
+  powers: ["Jump", "Beerpong"];
+  realName: "Tobbi";
+  alias: "Lola";
+  origin: "Island";
+  weakness: "boxing";
+  active: false;
+  isEvil: true;
+  height: 168;
+  birthday: {
+    day: 14;
+    month: 7;
+    year: 1998;
+  }
+
+  image: "https://www.pexels.com/photo/people-wearing-spider-man-adult-costume-walking-on-train-station-2859016/";
+}
